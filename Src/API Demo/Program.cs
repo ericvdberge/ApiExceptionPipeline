7fcb6d9b3a989b3f62898e51ce8cb5597dfbda8a
Eric.vandenberge@valiento.com <Eric.vandenberge@valiento.com>
2022-11-02 22:32:21 +0100

    better folder structure

---
+++ Src/API Demo/Program.cs
@@ -0,0 +1,32 @@
+using API_Demo.Exceptions;
+using ApiExceptionPipelineV2._0.Extensions;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseCustomExceptionPipeline(
+    exceptionDecoder: ExceptionDecoder.CustomExceptions,
+    exceptionMaps: ExceptionDecoder.CustomExceptionMaps
+);
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
